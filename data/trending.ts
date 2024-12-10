73996fa9157a5f0f9e2cc8ea3f54aadccae88962
amirkairzhan <amirkairzhan@bk.ru>
2024-12-10 18:55:00 +0800

    Add files via upload

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/1e/8b/6b/caption.jpg?w=1200&h=1200&s=1',
+        title: 'Dubai',
+        location: 'United Arab Emirates',
+        description: '15 Deals',
+    },
+    {
+        id: 2,
+        src: 'https://chabadcity.ru/wp-content/uploads/2021/06/welcome-1200x1080.jpg',
+        title: 'Moscow',
+        location: 'Russia',
+        description: '32 Deals',
+    },
+    {
+        id: 3,
+        src: 'https://media.istockphoto.com/id/499253740/photo/city-of-london-during-sunset-in-1x1-size.jpg?s=612x612&w=0&k=20&c=jaKgKCQyCz79TRPCqYH6_ryOeah0drAQoeuGd7n7lS8=',
+        title: 'London',
+        location: 'United Kingdom',
+        description: '45 Deals',
+    },
+    {
+        id: 4,
+        src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/13/2b/2f/caption.jpg?w=1200&h=1200&s=1',
+        title: 'Seoul',
+        location: 'South Korea',
+        description: '88 Deals',
+    },
+    {
+        id: 5,
+        src: 'https://knowledge.uli.org/-/media/images/market-spotlights/page-promo-images/new-york-1x1.jpg?h=1290&iar=0&mw=1290&w=1290&rev=591eb3ff258b4bb59a372bd0d1a515f5&hash=D984CDC6BD0FEA5BCDB7DB94A3F5B0C5',
+        title: 'New York City',
+        location: 'United States of America',
+        description: '92 Deals',
+    },
+];
